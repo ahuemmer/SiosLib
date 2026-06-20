8dcccea8b0ad68fa3940ecb52ae8a41613f04d7b
Andreas Hümmerich <andreas@huemmerich-web.de>
2026-06-20 19:22:27 +0200

    fix: remove unnecessary module dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.github.ahuemmer.sioslib {
     requires com.fazecast.jSerialComm;
     requires org.apache.logging.log4j;
-    requires org.jspecify;
 
     exports com.github.ahuemmer.sioslib;
 }
